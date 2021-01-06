169142e8a34bc864415c6fd04dec8f9cd4e5d34b
F4r3t <sultangareevalan@gmail.com>
2021-01-06 22:53:22 +0300

    Check main.py update

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import sys
 from random import randrange
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import QtCore, QtWidgets
 
 
-class Circle(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.btn = QtWidgets.QPushButton(Form)
+        self.btn.setGeometry(QtCore.QRect(210, 250, 75, 23))
+        self.btn.setObjectName("btn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn.setText(_translate("Form", "Кнопка"))
+
+
+class Circle(QWidget, Ui_Form):
     def __init__(self):
         super(Circle, self).__init__()
         self.do_paint = False
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
 
         self.btn.clicked.connect(self.paint)
 
@@ -26,7 +43,7 @@ class Circle(QWidget):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(randrange(256), randrange(256), randrange(256)))
         r = randrange(1, 300)
         qp.drawEllipse(0, 0, r, r)
 
